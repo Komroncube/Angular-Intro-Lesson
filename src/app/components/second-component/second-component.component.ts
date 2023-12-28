56609d4c5e1f9356150cb7d48c633da197660393
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-28 21:13:53 +0500

    second custom component

---
+++ src/app/components/second-component/second-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './second-component.component.html',
+  styleUrl: './second-component.component.css'
+})
+export class SecondComponentComponent {
+
+}
