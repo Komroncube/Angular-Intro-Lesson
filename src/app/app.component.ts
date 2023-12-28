5cee15dc0dff4eccbfd242f0f796ee2dc4afef2e
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-28 21:14:14 +0500

    importing modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { SecondComponentComponent } from "./components/second-component/second-component.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, FirstComponentComponent, SecondComponentComponent]
 })
 export class AppComponent {
   title = 'hello-angular-second-attempt';
