ca576d20ae7d99d8f7863d285f26bb5ac964779e
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-29 16:18:27 +0500

    testing components and tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { SecondComponentComponent } from "./components/second-component/second-component.component";
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, FirstComponentComponent, SecondComponentComponent]
+    imports: [CommonModule, RouterOutlet, FirstComponentComponent, SecondComponentComponent, MatSlideToggleModule]
 })
 export class AppComponent {
   title = 'hello-angular-second-attempt';
