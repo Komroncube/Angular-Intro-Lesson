a246775a772faba08dd06844040a07a229225d73
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-07 21:23:54 +0500

    Update app.component.html and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { SecondComponentComponent } from "./components/second-component/second-component.component";
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, FirstComponentComponent, SecondComponentComponent, MatSlideToggleModule]
+    imports: [CommonModule, RouterOutlet, FirstComponentComponent, SecondComponentComponent, MatSlideToggleModule, PipesComponent]
 })
 export class AppComponent {
   title = 'hello-angular-second-attempt';
