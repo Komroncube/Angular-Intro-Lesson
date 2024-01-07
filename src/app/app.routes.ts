f72a4c1c7c4b5b64f9ba0c455920fe69e76f83b2
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-07 21:06:45 +0500

    Add routes for pipes and directives components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pipes', component: PipesComponent,
+    },
+    {
+        path: 'directives', component: DirectivesComponent
+    },
+    {
+        path: '**', pathMatch: 'full', redirectTo: 'pipes'
+    }
+];
